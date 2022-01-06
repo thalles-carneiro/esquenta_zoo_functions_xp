62a8aba8b6bf04dd1176a543e846e5bc5ac177f4
Effolex <Effolex@gmail.com>
2022-01-06 09:36:23 -0300

    correção AttackMonster

--- src/attackMonster.js
+++ src/attackMonster.js
@@ -3,6 +3,7 @@ const data = require('../data/api_data');
 /*
   Desenvolva uma função que retone uma frase neste estilo:
   "Fulano, nivel X atacou o monstro Fuleco de tamanho Gigante dando X dano"
+  para capturar o dano, utilize o dano da primeira arma que o aventureiro possui
 */
 
 function attackMonster(adventurer, monster) {
--- src/getAdventurerHistory.js
+++ src/getAdventurerHistory.js
@@ -4,7 +4,7 @@ const data = require('../data/api_data');
   Desenvolva uma função que retorne uma lista de monstros,
   retorne um array de contendo o aventureiro,
   os monstros que ele derrotou e a arma, 
-  seguindo este formato [adventurer, weapon, monstersDefeated:[]]
+  seguindo este formato {adventurer: {}, weapons: {}, monstersToDefead:[]}
 */
 
 function getAdventurerHistory(adventurerId) {
