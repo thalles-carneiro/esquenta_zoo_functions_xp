d1f5214dee0ade3eaf64b58b66b7de94715ac6b8
Effolex <Effolex@gmail.com>
2022-01-06 08:16:18 -0300

    Adicionando requisitos adicionais

---
+++ src/attackMonster.js
@@ -0,0 +1,10 @@
+const data = require('../data/api_data');
+/*
+  Desenvolva uma função que retone uma frase neste estilo:
+  "Fulano, nivel X atacou o monstro Fuleco de tamanho Gigante dando X dano"
+*/
+function attackMonster(adventurer, monster, weapon) {
+  // escreva o código aqui
+}
+
+module.exports = attackMonster;
---
+++ src/getAdventurerHistory.js
@@ -0,0 +1,13 @@
+const data = require('../data/api_data');
+/*
+  Desenvolva uma função que retorne uma lista de monstros,
+  retorne um array de contendo o aventureiro,
+  os monstros que ele derrotou e a arma, 
+  seguindo este formato [adventurer, weapon, monstersDefeated:[]]
+*/
+function getAdventurerHistory(adventurer) {
+  // escreva o código aqui
+
+}
+
+module.exports = getMonstersByStats;
