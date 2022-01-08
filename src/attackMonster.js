3094fa7b85a438e327d4bb430d6c8334a9c7442c
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-08 19:06:39 -0300

    funcao attackMonster resolvido

--- src/attackMonster.js
+++ src/attackMonster.js
@@ -1,4 +1,4 @@
-const data = require('../data/api_data');
+const { monsters, adventurers, weapons } = require('../data/api_data');
 
 /*
   Desenvolva uma função que retone uma frase neste estilo:
@@ -6,8 +6,19 @@ const data = require('../data/api_data');
   para capturar o dano, utilize o dano da primeira arma que o aventureiro possui
 */
 
-function attackMonster(adventurer, monster) {
-  // escreva o código aqui
+function attackMonster(adventurerId, monsterId) {
+  const { name, level, weapons: [weaponId] } = adventurers.find((v) => v.id === adventurerId);
+  const { name: monster, size } = monsters.find((v) => v.id === monsterId);
+  const { damage } = weapons.find((v) => v.id === weaponId);
+
+  return `${name}, nivel ${level} atacou o monstro ${monster} de tamanho ${size}\
+ dando ${damage} dano`;
 }
+const adventurer = 'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1';
+const monster = 'baa6e93a-f295-44e7-8f70-2bcdc6f6948d';
+const actual = attackMonster(adventurer, monster);
+console.log(actual);
+
+// const expected = 'Nigel Bethea, nivel 5 atacou o monstro Acolyte de tamanho Medium dando 15 dano';
 
 module.exports = attackMonster;
