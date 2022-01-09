ae5df1980bbdcb21478bae529444e4694cc5d3cf
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-08 21:19:56 -0300

    funcao getAdventurerHistory resolvido

--- src/getAdventurerHistory.js
+++ src/getAdventurerHistory.js
@@ -1,14 +1,27 @@
-const data = require('../data/api_data');
+const { adventurers, weapons: weaponsData, monsters } = require('../data/api_data');
 
 /*
   Desenvolva uma função que retorne uma lista de monstros,
   retorne um array de contendo o aventureiro,
-  os monstros que ele derrotou e a arma, 
+  os monstros que ele derrotou e a arma,
   seguindo este formato {adventurer: {}, weapons: {}, monstersToDefead:[]}
 */
 
 function getAdventurerHistory(adventurerId) {
-  // escreva o código aqui
+  return adventurers.reduce((acc, adventurer) => {
+    if (adventurer.id === adventurerId) {
+      const weapons = weaponsData.filter((v) => adventurer.weapons.includes(v.id));
+      const monstersToDefead = monsters.filter((v) => !adventurer.defeatedMonsters.includes(v.id));
+
+      const finalObj = {
+        adventurer,
+        weapons,
+        monstersToDefead,
+      };
+      return finalObj;
+    }
+    return acc;
+  }, {});
 }
 
 module.exports = getAdventurerHistory;
